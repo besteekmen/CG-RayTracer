4f9d35cf65cdfa9fd9be704a5f82e4329594feb6
rani00002 <rani00002@uni-saarland.de>
2021-11-17 17:10:15 +0100

    Upload files to 'rt'

---
+++ rt/intersection.cpp
@@ -0,0 +1,44 @@
+#include <rt/intersection.h>
+
+namespace rt {
+
+Intersection::Intersection(float distance, const Ray& ray, const Solid* solid, const Vector& normal, const Point& local)
+{
+    /* TODO */
+    this->distance = distance;
+    this->ray = ray;
+    this->solid = solid;
+    this->normal_vector = normal;
+    this->local_hit_Point = local;
+    this->isNull = false;
+}
+
+Intersection::operator bool() const {
+    /* TODO */
+    if (this->isNull) {
+        return(false);
+    }
+    return(true);
+}
+
+Intersection Intersection::failure() {
+    /* TODO */
+    return(Intersection());
+}
+
+Point Intersection::hitPoint() const {
+    /* TODO */
+    return(this->hit_Point);
+}
+
+Vector Intersection::normal() const {
+    /* TODO */
+    return(this->normal_vector);
+}
+
+Point Intersection::local() const {
+    /* TODO */
+    return(this->local_hit_Point);
+}
+
+}
