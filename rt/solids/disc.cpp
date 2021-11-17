47e12720d553abaec084b73d16231a8d31fcb9b2
rani00002 <rani00002@uni-saarland.de>
2021-11-17 17:16:05 +0100

    Upload files to 'rt/solids'

---
+++ rt/solids/disc.cpp
@@ -0,0 +1,44 @@
+#include <rt/solids/disc.h>
+
+namespace rt {
+
+Disc::Disc(const Point& center, const Vector& normal, float radius, CoordMapper* texMapper, Material* material): Solid(texMapper, material)
+{
+    /* TODO */
+    this->center = center;
+    this->radius = radius;
+    this->normal = normal;
+}
+
+BBox Disc::getBounds() const {
+    /* TODO */ NOT_IMPLEMENTED;
+}
+
+Intersection Disc::intersect(const Ray& ray, float previousBestDistance) const {
+    /* TODO */
+    float denominator = dot(this->normal, ray.d);
+    if (abs(denominator) <= std::numeric_limits<float>::epsilon()) {
+        return(Intersection::failure());
+    }
+    float distance = dot(this->normal, this->center - ray.o) / denominator;
+    if (distance > 0) {
+        float distance_center_to_point = (ray.getPoint(distance) - this->center).length();
+        if (distance_center_to_point > this->radius) {
+            return(Intersection::failure());
+        }
+        return(Intersection(distance, ray, this, this->normal, ray.getPoint(distance)));
+    }
+    return(Intersection::failure());
+
+}
+
+Solid::Sample Disc::sample() const {
+    NOT_IMPLEMENTED;
+}
+
+float Disc::getArea() const {
+    /* TODO */
+    return(pi * pow(this->radius, 2));
+}
+
+}
---
+++ rt/solids/infiniteplane.cpp
@@ -0,0 +1,41 @@
+#include <rt/solids/infiniteplane.h>
+#include<cmath>
+
+namespace rt {
+
+InfinitePlane::InfinitePlane(const Point& origin, const Vector& normal, CoordMapper* texMapper, Material* material): Solid(texMapper, material)
+{
+    /* TODO */
+    this->origin = origin;
+    this->normal = normal;
+}
+
+BBox InfinitePlane::getBounds() const {
+    /* TODO */ NOT_IMPLEMENTED;
+}
+
+Intersection InfinitePlane::intersect(const Ray& ray, float previousBestDistance) const {
+    /* TODO */
+    float denominator = dot(this->normal, ray.d);
+    if (abs(denominator) <= std::numeric_limits<float>::epsilon()) {
+        return(Intersection::failure());
+    }
+    float distance = dot(this->normal, this->origin - ray.o)/denominator;
+    if (distance > 0) {
+        return(Intersection(distance, ray, this, this->normal, ray.getPoint(distance)));
+    }
+    return(Intersection::failure());
+    
+
+}
+
+Solid::Sample InfinitePlane::sample() const {
+    /* TODO */ NOT_IMPLEMENTED;
+}
+
+float InfinitePlane::getArea() const {
+    /* TODO */ NOT_IMPLEMENTED;
+    return(FLT_MAX);
+}
+
+}
