709f92be38c8526471c91cb66d1d40a680841ca0
rani00002 <rani00002@uni-saarland.de>
2021-11-17 17:15:20 +0100

    Upload files to 'rt/integrators'

---
+++ rt/integrators/casting.cpp
@@ -0,0 +1,16 @@
+#include <rt/integrators/casting.h>
+#include <rt/world.h>
+#include<cmath>
+
+namespace rt {
+
+    RGBColor RayCastingIntegrator::getRadiance(const Ray& ray) const {
+        /* TODO */
+        Intersection intersect = this->world->scene->intersect(ray, FLT_MAX);
+        if (intersect) {
+            float grey_color = -dot(intersect.normal(), ray.d);
+            return(RGBColor(grey_color, grey_color, grey_color));
+        }
+        return(RGBColor(0.0f, 0.0f, 0.0f));
+    }
+}
