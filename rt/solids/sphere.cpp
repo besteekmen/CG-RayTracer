2bf586339a82fdf7dbbc189bb430d8b1d788e379
s8beekme <s8beekme@stud.uni-saarland.de>
2021-11-19 21:43:18 +0100

    Attempt to fix sphere tests

--- rt/solids/sphere.cpp
+++ rt/solids/sphere.cpp
@@ -14,42 +14,33 @@ BBox Sphere::getBounds() const {
     /* TODO */ NOT_IMPLEMENTED;
 }
 
+bool is_t_valid(float t, float previousBestDistance){
+    return (t < previousBestDistance && t >= std::numeric_limits<float>::epsilon());
+}
+
 Intersection Sphere::intersect(const Ray& ray, float previousBestDistance) const {
+    float A = dot(ray.d, ray.d);
+		float B = 2.0f * dot(ray.o - center, ray.d);
+		float C = dot(ray.o - center, ray.o - center) - (radius * radius);
+		float t, d = B * B - 4 * A * C;
+    Vector normal;
 
-		float A = dot (ray.d, ray.d);
-		float B = 2 * dot(ray.d, (ray.o - Point(0,0,0))) - 2 * dot((center - Point(0,0,0)), ray.d);
-		float C =  (ray.o - Point(0,0,0)).lensqr() - 2 * dot ((center - Point(0,0,0)), (ray.o - Point(0,0,0))) + (center - Point(0,0,0)).lensqr() - radius * radius;
+		if (d < 0.0f) return Intersection::failure();
+    else {
+  			float t1 = (- B + sqrt(d)) / (2 * A);
+  			float t2 = (- B - sqrt(d)) / (2 * A);
 
-		float t;
+  			bool is_t1_valid = is_t_valid(t1, previousBestDistance);
+  			bool is_t2_valid = is_t_valid(t2, previousBestDistance);
 
-		if (A == 0){
-			t = - C / B;
-      Vector n = -(this->center - ray.getPoint(t)).normalize();
-			if (t <= previousBestDistance && t >= 0) return Intersection(t, ray, this, n, ray.getPoint(t));
+  			if (!is_t1_valid && !is_t2_valid) return Intersection::failure();
+  			if (is_t1_valid && is_t2_valid) t = min(t1, t2);
+  			if (!is_t1_valid && is_t2_valid) t = t2;
+  			if (is_t1_valid && !is_t2_valid) t = t1;
 		}
-		else
-		{
-			float d = B * B - 4 * A * C;
 
-			if (d < 0.0) return Intersection::failure();
-
-			float t1 = ( - B + sqrt(d)) / (2 * A);
-			float t2 = ( - B - sqrt(d)) / (2 * A);
-
-			bool is_t1_valid = (t1 <= previousBestDistance && t >= 0);
-			bool is_t2_valid = (t2 <= previousBestDistance && t >= 0);
-
-			if (!is_t1_valid && !is_t2_valid) return Intersection::failure();
-
-			float t;
-
-			if (is_t1_valid && is_t2_valid) t = min(t1, t2);
-			if (!is_t1_valid && is_t2_valid) t = t2;
-			if (is_t1_valid && !is_t2_valid) t = t1;
-
-      Vector n = -(this->center - ray.getPoint(t)).normalize();
-			return Intersection(t, ray, this, n, ray.getPoint(t));
-		}
+    normal = ray.getPoint(t) - center;
+    return Intersection(t, ray, this, normal.normalize(), ray.getPoint(t));
 }
 
 Solid::Sample Sphere::sample() const {
