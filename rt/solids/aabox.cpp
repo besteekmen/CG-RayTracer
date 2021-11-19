2d566144b0656755569e5dbd752a82a8a299feea
s8beekme <s8beekme@stud.uni-saarland.de>
2021-11-19 22:05:41 +0100

    Attempt to fix aabox test

--- rt/solids/aabox.cpp
+++ rt/solids/aabox.cpp
@@ -1,58 +1,15 @@
 #include <rt/solids/aabox.h>
-
+#include <core/point.h>
 namespace rt {
 
-AABox::AABox(const Point& corner1, const Point& corner2, CoordMapper* texMapper, Material* material): Solid(texMapper, material)
+AABox::AABox(const Point& corner1, const Point& corner2, CoordMapper* texMapper, Material* material)
 {
-    /* TODO */
-    this->corners[0] = corner1;
-    this->corners[1] = corner2;
-    this->normals[0] = Vector(1, 0, 0);
-    this->normals[1] = Vector(-1, 0, 0);
-    this->normals[2] = Vector(0, 1, 0);
-    this->normals[3] = Vector(0, -1, 0);
-    this->normals[4] = Vector(0, 0, 1);
-    this->normals[5] = Vector(0, 0, -1);
-    if (corners[0].x < corners[1].x) {
-        this->normal_associated_point_index[0] = 1;
-        this->normal_associated_point_index[1] = 0;
-        max[0] = corners[1].x;
-        min[0] = corners[0].x;
-    }
-    else {
-        this->normal_associated_point_index[0] = 0;
-        this->normal_associated_point_index[1] = 1;
-        max[0] = corners[0].x;
-        min[0] = corners[1].x;
-    }
-    if (corners[0].y < corners[1].y) {
-        this->normal_associated_point_index[2] = 1;
-        this->normal_associated_point_index[3] = 0;
-        max[1] = corners[1].y;
-        min[1] = corners[0].y;
-    }
-    else {
-        this->normal_associated_point_index[2] = 0;
-        this->normal_associated_point_index[3] = 1;
-        max[1] = corners[0].y;
-        min[1] = corners[1].y;
-    }
-    if (corners[0].z < corners[1].z) {
-        this->normal_associated_point_index[4] = 1;
-        this->normal_associated_point_index[5] = 0;
-        max[2] = corners[1].z;
-        min[2] = corners[0].z;
-    }
-    else {
-        this->normal_associated_point_index[4] = 0;
-        this->normal_associated_point_index[5] = 1;
-        max[2] = corners[0].z;
-        min[2] = corners[1].z;
-    }
+    this->corner1 = corner1;
+    this->corner2 = corner2;
 }
 
 BBox AABox::getBounds() const {
-    /* TODO */ NOT_IMPLEMENTED;
+    NOT_IMPLEMENTED;
 }
 
 Solid::Sample AABox::sample() const {
@@ -60,48 +17,78 @@ Solid::Sample AABox::sample() const {
 }
 
 float AABox::getArea() const {
-    /* TODO */
-    Vector v = (corners[0] - corners[1]);
-    return(2*(abs(v.x*v.y)+abs(v.y*v.z)+abs(v.z*v.x)));
+    float x = std::abs(corner1.x - corner2.x);
+    float y = std::abs(corner1.y - corner2.y);
+    float z = std::abs(corner1.z - corner2.z);
+    return 2 * (x*y + y*z + z*x) ;
+}
+
+bool is_t_valid(float t, float previousBestDistance){
+    return (t < previousBestDistance && t >= std::numeric_limits<float>::epsilon());
 }
 
 Intersection AABox::intersect(const Ray& ray, float previousBestDistance) const {
-    /* TODO */
-    float distance = 1000;
-    int least_distance_index = -1;
-    for (int i = 0; i < 6; i++) {
-        float denominator = dot(ray.d, this->normals[i]);
-        if (abs(denominator) <= std::numeric_limits<float>::epsilon()) {
-            continue;
+    // We compute t_near (t0) and t_far (t1) for intersection with the aabox
+    // ray.o.x + t0x * ray.d.x = corner1.x
+    float t0x = (corner1.x - ray.o.x) / ray.d.x;
+    float t1x = (corner2.x - ray.o.x) / ray.d.x;
+
+    float t0y = (corner1.y - ray.o.y) / ray.d.y;
+    float t1y = (corner2.y - ray.o.y) / ray.d.y;
+
+    float t0z = (corner1.z - ray.o.z) / ray.d.z;
+    float t1z = (corner2.z - ray.o.z) / ray.d.z;
+
+    if (t0x > t1x) std::swap(t0x, t1x);
+    if (t0y > t1y) std::swap(t0y, t1y);
+    if (t0z > t1z) std::swap(t0z, t1z);
+
+    // Now we get our intersection points.
+    float t0 = max(max(t0x, t0y), t0z);
+    float t1 = min(min(t1x, t1y), t1z);
+    if (t0 >= t1)
+        return Intersection::failure();
+
+    bool is_t0_valid = is_t_valid(t0, previousBestDistance);
+    bool is_t1_valid = is_t_valid(t1, previousBestDistance);
+
+    if (!is_t0_valid && !is_t1_valid)
+        return Intersection::failure();
+
+    float t;
+    if (is_t0_valid && is_t1_valid) t = min(t0, t1);
+    if (!is_t0_valid && is_t1_valid) t = t1;
+    if (is_t0_valid && !is_t1_valid) t = t0;
+
+    // Prepare our normal vector
+    Vector normal;
+    if (t == t0) {
+        if (t == t0x){
+            normal = Vector(-1.f, 0, 0);
         }
-        float dist = dot(corners[this->normal_associated_point_index[i]] - ray.o, this->normals[i])/denominator;
-        if (dist>0 && dist < distance && dist<previousBestDistance) {
-            Point pt_of_intersection = ray.getPoint(dist);
-            bool flag = true;
-            if (this->normals[i].x ==0) {
-                if (pt_of_intersection.x > max[0] || pt_of_intersection.x < min[0]) {
-                    flag = false;
-                }
-            }
-            if (this->normals[i].y ==0) {
-                if (pt_of_intersection.y > max[1] || pt_of_intersection.y < min[1]) {
-                    flag = false;
-                }
-            }
-            if (this->normals[i].z ==0) {
-                if (pt_of_intersection.z > max[2] || pt_of_intersection.z < min[2]) {
-                    flag = false;
-                }
-            }
-            if (flag) {
-                distance = dist;
-                least_distance_index = i;
-            }
+        if (t == t0y) {
+            normal = Vector(0, -1.f, 0);
         }
+        if (t == t0z) {
+            normal = Vector(0, 0, 1.f);
+        }
+        return Intersection(t, ray, this, normal, ray.getPoint(t));
     }
-    if (least_distance_index != -1 && distance > 0) {
-        return(Intersection(distance, ray, this, this->normals[least_distance_index], ray.getPoint(distance)));
+
+
+    if (t == t1) {
+        if (t == t1x) {
+            normal = Vector(1.f, 0, 0);
+        }
+        if (t == t1y) {
+            normal = Vector(0, 1.f, 0);
+        }
+        if (t == t1z) {
+            normal = Vector(0, 0, -1.f);
+        }
+        return Intersection(t, ray, this, normal, ray.getPoint(t));
     }
-    return(Intersection::failure());
+    return Intersection::failure();
 }
+
 }
