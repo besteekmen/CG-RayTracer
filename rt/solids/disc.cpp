24262382ceea0745da049181f6cca6f751c2f0e2
s8beekme <s8beekme@stud.uni-saarland.de>
2021-11-19 17:43:35 +0100

    Uploading final solids

---
+++ rt/solids/disc.cpp
@@ -0,0 +1,40 @@
+#include <rt/solids/disc.h>
+
+namespace rt {
+
+Disc::Disc(const Point& center, const Vector& normal, float radius, CoordMapper* texMapper, Material* material)
+{
+		this->center = center;
+		this->normal = normal;
+		this->radius = radius;
+}
+
+BBox Disc::getBounds() const {
+    /* TODO */ NOT_IMPLEMENTED;
+}
+
+Intersection Disc::intersect(const Ray& ray, float previousBestDistance) const {
+
+    if (dot(ray.d, this->normal) == 0.0) return Intersection::failure();
+
+    float t = - dot(ray.o - center, this->normal) / dot(ray.d, this->normal);
+
+    if (t > previousBestDistance || t < 0) return Intersection::failure();
+
+    Point hit_point = ray.getPoint(t);
+
+    if ((hit_point - center).length() > radius)
+        return Intersection::failure();
+
+    return Intersection(t, ray, this, this->normal, hit_point);
+}
+
+Solid::Sample Disc::sample() const {
+    NOT_IMPLEMENTED;
+}
+
+float Disc::getArea() const {
+    return pi * pow(radius,2);
+}
+
+}
