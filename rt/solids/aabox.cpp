759e2f6089a13bddfaea99241420c47016b16648
s8beekme <s8beekme@stud.uni-saarland.de>
2021-12-01 00:53:05 +0100

    Simplified aabox intersection

--- rt/solids/aabox.cpp
+++ rt/solids/aabox.cpp
@@ -23,44 +23,41 @@ float AABox::getArea() const {
   return 2 * (x*y + y*z + z*x) ;
 }
 
-bool is_t_valid_aa(float t, float previousBestDistance){
-    return (t < previousBestDistance && t >= std::numeric_limits<float>::epsilon());
-}
-
 Intersection AABox::intersect(const Ray& ray, float previousBestDistance) const {
-  float ray_dir_x = 1 / ray.d.x;
-  float ray_dir_y = 1 / ray.d.y;
-  float ray_dir_z = 1 / ray.d.z;
 
-  // For X
-  float tx_1 = (minCorner.x - ray.o.x) * ray_dir_x;
-  float tx_2 = (maxCorner.x - ray.o.x) * ray_dir_x;
+  /*float rayDirX = 1 / ray.d.x;
+  float rayDirY = 1 / ray.d.y;
+  float rayDirZ = 1 / ray.d.z;
+  assert(rayDirX && rayDirY && rayDirZ);
 
-  // For Y
-  float ty_3 = (minCorner.y - ray.o.y) * ray_dir_y;
-  float ty_4 = (maxCorner.y - ray.o.y) * ray_dir_y;
+  float t0x = (minCorner.x - ray.o.x) * rayDirX;
+  float t1x = (maxCorner.x - ray.o.x) * rayDirX;
 
-  // For Z
-  float tz_5 = (minCorner.z - ray.o.z) * ray_dir_z;
-  float tz_6 = (maxCorner.z - ray.o.z) * ray_dir_z;
+  float t0y = (minCorner.y - ray.o.y) * rayDirY;
+  float t1y = (maxCorner.y - ray.o.y) * rayDirY;
 
-  float t_min = std::max(
-    std::max(std::min(tx_1, tx_2), std::min(ty_3, ty_4)),
-    std::min(tz_5, tz_6));
-  float t_max = std::min(
-    std::min(std::max(tx_1, tx_2), std::max(ty_3, ty_4)),
-    std::max(tz_5, tz_6));
+  float t0z = (minCorner.z - ray.o.z) * rayDirZ;
+  float t1z = (maxCorner.z - ray.o.z) * rayDirZ;*/
 
-  if (t_max < 0 || t_min > t_max || t_max > FLT_MAX) { return Intersection::failure(); };
-  if (t_min > previousBestDistance || t_min < 0) { return Intersection::failure(); };
+  // ray.o.x + t0x * ray.d.x = corner1.x
+  Vector near = (minCorner - ray.o) / ray.d;
+  Vector far = (maxCorner - ray.o) / ray.d;
+
+  float t_min = max(max(min(near.x, far.x),min(near.y, far.y)),min(near.z, far.z));
+  float t_max = min(min(max(near.x, far.x),max(near.y, far.y)),max(near.z, far.z));
+
+  if (t_max < 0 || t_min > t_max || t_max > FLT_MAX)
+    return Intersection::failure();
+  if (t_min > previousBestDistance || t_min < 0)
+    return Intersection::failure();
 
   Vector n;
-  n = t_min == tx_1 ? Vector(-1, 0, 0) : n;
-  n = t_min == tx_2 ? Vector(1, 0, 0) : n;
-  n = t_min == ty_3 ? Vector(0, -1, 0) : n;
-  n = t_min == ty_4 ? Vector(0, 1, 0) : n;
-  n = t_min == tz_5 ? Vector(0, 0, -1) : n;
-  n = t_min == tz_6 ? Vector(0, 0, 1) : n;
+  n = t_min == near.x ? Vector(-1, 0, 0) : n;
+  n = t_min == far.x ? Vector(1, 0, 0) : n;
+  n = t_min == near.y ? Vector(0, -1, 0) : n;
+  n = t_min == far.y ? Vector(0, 1, 0) : n;
+  n = t_min == near.z ? Vector(0, 0, -1) : n;
+  n = t_min == far.z ? Vector(0, 0, 1) : n;
 
   return Intersection(t_min, ray, this, n, ray.getPoint(t_min));
 }
