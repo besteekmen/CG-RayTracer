81f84cb8349f590a1c00f9d99e3c3524ac89c3ec
s8beekme <s8beekme@stud.uni-saarland.de>
2021-12-01 08:00:03 +0100

    Bbox update for infinite plane and disc

--- rt/solids/infiniteplane.cpp
+++ rt/solids/infiniteplane.cpp
@@ -2,24 +2,23 @@
 
 namespace rt {
 
-  InfinitePlane::InfinitePlane(const Point& origin, const Vector& normal, CoordMapper* texMapper, Material* material)
-  {
-    this->origin = origin;
-    this->normal = normal;
-    this->texMapper = texMapper;
-    this->material = material;
-  }
+InfinitePlane::InfinitePlane(const Point& origin, const Vector& normal, CoordMapper* texMapper, Material* material)
+{
+  this->origin = origin;
+  this->normal = normal;
+  this->texMapper = texMapper;
+  this->material = material;
+}
 
-  BBox InfinitePlane::getBounds() const {
-      /* TODO */ NOT_IMPLEMENTED;
-  }
+BBox InfinitePlane::getBounds() const {
+  return BBox::full();
+}
 
 Intersection InfinitePlane::intersect(const Ray& ray, float previousBestDistance) const {
-
-		if (dot(ray.d, this->normal) == 0.0) return Intersection::failure();
-		float t = - dot(ray.o - origin, normal) / dot(ray.d, this->normal);
-		if (t > previousBestDistance || t < 0) return Intersection::failure();
-		return Intersection(t, ray, this, this->normal, ray.getPoint(t));
+  if (dot(ray.d, this->normal) == 0.0) return Intersection::failure();
+	float t = - dot(ray.o - origin, normal) / dot(ray.d, this->normal);
+	if (t > previousBestDistance || t < 0) return Intersection::failure();
+	return Intersection(t, ray, this, this->normal, ray.getPoint(t));
 }
 
 Solid::Sample InfinitePlane::sample() const {
