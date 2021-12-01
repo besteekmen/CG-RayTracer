1ae941affd93e7b6f2b337cd5d97e669a858151c
s8beekme <s8beekme@stud.uni-saarland.de>
2021-12-01 07:34:20 +0100

    Initial bbox implementation

--- rt/bbox.cpp
+++ rt/bbox.cpp
@@ -3,28 +3,61 @@
 namespace rt {
 
 BBox BBox::empty() {
-    /* TODO */ NOT_IMPLEMENTED;
+  BBox bbox;
+  bbox.isEmpty = true;
+  return bbox;
 }
 
 BBox BBox::full() {
-    /* TODO */ NOT_IMPLEMENTED;
+  Point minimum = Point(__FLT_MIN__, __FLT_MIN__, __FLT_MIN__);
+  Point maximum = Point(__FLT_MAX__, __FLT_MAX__, __FLT_MAX__);
+  return BBox(minimum, maximum);
 }
 
-
 void BBox::extend(const Point& point) {
-    /* TODO */ NOT_IMPLEMENTED;
+  if (isEmpty) {
+    isEmpty = false;
+    min = point;
+    max = point;
+    return;
+  }
+  min = min(this->min, point);
+  max = max(this->max, point);
+  return;
 }
 
 void BBox::extend(const BBox& bbox) {
-    /* TODO */ NOT_IMPLEMENTED;
+  if (isEmpty) {
+    isEmpty = bbox.isEmpty;
+    min = bbox.min;
+    max = bbox.max;
+    return;
+  }
+  min = min(this->min, bbox.min);
+  max = max(this->max, bbox.max);
+  return;
 }
 
 std::pair<float, float> BBox::intersect(const Ray& ray) const {
-    /* TODO */ NOT_IMPLEMENTED;
+  // element-wise division for two vectors (included in vector class)
+  Vector near = (min - ray.o) / ray.d;
+  Vector far = (max - ray.o) / ray.d;
+
+  float t_min = max(max(min(near.x, far.x),min(near.y, far.y)),min(near.z, far.z));
+  float t_max = min(min(max(near.x, far.x),max(near.y, far.y)),max(near.z, far.z));
+
+  return std::make_pair (t_min, t_max);
 }
 
 bool BBox::isUnbound() const {
-    /* TODO */ NOT_IMPLEMENTED;
+  Point minimum = min(min, max);
+  Point maximum = max(min, max);
+  return (minimum.x == -FLT_MAX || minimum.y == -FLT_MAX || minimum.z == -FLT_MAX ||
+    maximum.x == FLT_MAX || maximum.y == FLT_MAX || maximum.z == FLT_MAX);
+}
+
+Point BBox::centroid() const {
+  return Point((min.x + max.x)/2, (min.y + max.y)/2, (min.z + max.z)/2);
 }
 
 }
--- rt/bbox.h
+++ rt/bbox.h
@@ -13,10 +13,12 @@ class BBox {
 public:
     Point min, max;
 
-    BBox() {}
+    BBox() {/*this->isEmpty = true;*/}
     BBox(const Point& min, const Point& max)
     {
-        /* TODO */
+      this->min = min;
+      this->max = max;
+      this->isEmpty = false;
     }
 
     static BBox empty();
@@ -26,16 +28,22 @@ public:
     void extend(const BBox& bbox);
 
     Vector diagonal() const {
-        /* TODO */ NOT_IMPLEMENTED;
+      return max-min;
     }
 
     float area() const {
-        /* TODO */ NOT_IMPLEMENTED;
+      float x = std::abs(min.x - max.x);
+      float y = std::abs(min.y - max.y);
+      float z = std::abs(min.z - max.z);
+      return 2 * (x*y + y*z + z*x);
     }
 
     std::pair<float, float> intersect(const Ray& ray) const;
 
     bool isUnbound() const;
+    Point centroid() const;
+    //int largestAxisIndex();
+    bool isEmpty;
 };
 
 }
