62396cd577bd532282238a614b299718872682f3
s8beekme <s8beekme@stud.uni-saarland.de>
2022-01-14 07:40:16 +0100

    Update camera for motion

--- rt/cameras/perspective.cpp
+++ rt/cameras/perspective.cpp
@@ -1,9 +1,10 @@
 #include <rt/cameras/perspective.h>
 #include <math.h>
+#include <core/random.h>
 
 namespace rt {
 
-PerspectiveCamera::PerspectiveCamera(const Point& center, const Vector& forward, const Vector& up, float verticalOpeningAngle, float horizontalOpeningAngle)
+PerspectiveCamera::PerspectiveCamera(const Point& center, const Vector& forward, const Vector& up, float verticalOpeningAngle, float horizontalOpeningAngle, float time0, float time1)
 {
     this->center = center;
   	this->forward = forward;
@@ -18,12 +19,14 @@ PerspectiveCamera::PerspectiveCamera(const Point& center, const Vector& forward,
     this->CamXAxis = cross(this->forward, this->up.normalize()).normalize();
     this->CamYAxis = cross(this->CamXAxis, this->CamZAxis).normalize();
 
+    this->time0 = time0;
+    this->time1 = time1;
 }
 
 Ray PerspectiveCamera::getPrimaryRay(float x, float y) const {
 		Vector dir = (x * CamXAxis * scaleX + y * CamYAxis * scaleY +  CamZAxis).normalize();
-		return Ray(center, dir);
-
+    float t = time0 + random() * (time1 - time0);
+		return Ray(center, dir, t);
 }
 
 }
