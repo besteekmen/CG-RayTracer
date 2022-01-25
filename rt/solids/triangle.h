06a0205105a614e06411d109ba382e1f6423a049
s8beekme <s8beekme@stud.uni-saarland.de>
2022-01-25 16:44:36 +0100

    Update triangle header for public elements

--- rt/solids/triangle.h
+++ rt/solids/triangle.h
@@ -15,7 +15,6 @@ public:
     virtual Intersection intersect(const Ray& ray, float previousBestDistance = FLT_MAX) const;
     virtual Sample sample() const;
     virtual float getArea() const;
-private:
     Point v1;
   	Point v2;
   	Point v3;
